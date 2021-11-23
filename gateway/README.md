42b435a204f19a1e6afd4909ecee145a3819a028
takwa <takwa.gnenna@outlook.com>
2021-11-23 23:01:01 +0100

    restructuring the config files: gateway-live

--- gateway/README.md
+++
@@ -1,1 +0,0 @@
-# Infrastructure_As_Code
--- gateway/remote-backend/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key_id" {}
-
-variable "aws_secret_access_key" {}
-
-variable "region" {}
-
-variable "environment" {
-  description = "dev, test or prod env"
-}
