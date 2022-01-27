8ea123cec04356be1ea7313d5692b261b63297b6
Takwa <takwa.gnenna@outlook.com>
2022-01-27 15:08:48 +0100

    subnet id format fix

--- redis_ec2/main.tf
+++ redis_ec2/main.tf
@@ -16,6 +16,7 @@ locals {
     Automation  = "true"
     Owner       = "DevOps team"
     } 
+    subnet_ids  = tolist(data.aws_subnet_ids.gateway_subnets_ids)
 }
 
 module "redis_cli" {
@@ -73,7 +74,7 @@ module "redis_cli" {
         description                 = "eth0"
         device_index                = 0
         security_groups             = [data.aws_security_group.ecs_sg.id]
-        subnet_id                   = data.aws_subnet_ids.gateway_subnets_ids
+        subnet_id                   = local.subnet_id[0]
         associate_public_ip_address = true
         },
     ]
