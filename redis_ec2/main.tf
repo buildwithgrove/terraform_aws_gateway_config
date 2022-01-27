7bc998417c117835ee5ad63aa7e555c7d030ef9c
Takwa <takwa.gnenna@outlook.com>
2022-01-27 14:02:11 +0100

    specify one subnet to launch in

--- redis_ec2/main.tf
+++ redis_ec2/main.tf
@@ -72,7 +72,7 @@ module "redis_cli" {
         description                 = "eth0"
         device_index                = 0
         security_groups             = [data.aws_security_group.ecs_sg.id]
-        subnet_id                   = data.aws_subnet_ids.gateway_subnets_ids[0]
+        subnet_id                   = tolist(data.aws_subnet_ids.gateway_subnets_ids)[0]
         associate_public_ip_address = true
         },
     ]
