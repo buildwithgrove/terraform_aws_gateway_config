764a85ed27d02042852e755e61736f46df5c08ec
Takwa <takwa.gnenna@outlook.com>
2022-02-25 14:44:04 +0100

    modify max depolyment max %

--- demo/main.tf
+++ demo/main.tf
@@ -432,7 +432,7 @@ module "gateway" {
   desired_count                       = 3 
   scheduling_strategy 			        	= "REPLICA"
   deployment_minimum_healthy_percent 	= 50
-  deployment_maximum_percent			    = 50	
+  deployment_maximum_percent			    = 100	
   health_check_grace_period_seconds	  = 90
   
   load_balancer = [{
