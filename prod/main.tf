384342f8b166f21e2ce70e928a92a4c8e6c83ad1
Takwa <takwa.gnenna@outlook.com>
2022-03-11 14:30:39 +0100

    add aws access keys to the second provider

--- prod/main.tf
+++ prod/main.tf
@@ -20,6 +20,8 @@ provider "aws" {
 provider "aws" {
   alias      = "peer"
   region     = "us-west-2"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
 module "alb_sg" {
